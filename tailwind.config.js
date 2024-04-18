c53f6fd660300892d7c4df4d9f74f887eeda31a1
Alan Lima <dev.alanlima@gmail.com>
2024-04-18 20:30:11 -0300

    feat: Create solid shadow in TailWindCss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'solid': '2px 2px 0px 0px rgba(0,0,0,1)'
+      }
+    },
   },
   plugins: [],
 }
