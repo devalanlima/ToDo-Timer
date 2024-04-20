0c93d2bd6f030444bb431bff2b2a964171d1d576
Alan Lima <dev.alanlima@gmail.com>
2024-04-20 17:29:30 -0300

    feat: Add logic to fetch the theme from local storage

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
 
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+
+const colorTheme = ref<string | null>(localStorage.getItem('theme'));
+
+colorTheme.value !== null && document.documentElement.classList.add(colorTheme.value);
 </script>
-
-<style scoped>
-
-</style>
