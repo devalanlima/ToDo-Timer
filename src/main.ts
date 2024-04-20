fa9e5e017c4dc8f6548ba3b3a0a128d75c172f74
Alan Lima <dev.alanlima@gmail.com>
2024-04-20 19:25:00 -0300

    build: Add uuid

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import uuid from "vue-uuid";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
+app.use(uuid)
+
