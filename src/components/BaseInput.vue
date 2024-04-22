290d4afa5775c22fcf71dfd48681d09f771ef85f
Alan Lima <dev.alanlima@gmail.com>
2024-04-22 19:21:49 -0300

    feat: Create Base Input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input class="outline-none focus:bg-gray-300 grow shadow-solid border-2 border-black py-2 px-4 m-1" v-model="inputValue">
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  modelValue: any | undefined;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+  'update:modelValue': [value: typeof props.modelValue];
+}>()
+
+const inputValue = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(inputValue) {
+    emit('update:modelValue', inputValue);
+  }
+})
+
+</script>
