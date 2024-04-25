b4a2b5559a511151f3240c0b9d57e67c262124a9
Alan Lima <dev.alanlima@gmail.com>
2024-04-25 12:44:15 -0300

    feat: Create dark mode button

---
+++ src/components/DarkmodeButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <BaseButton
+    icon="fa-solid fa-moon"
+    @@click="darkMode"
+    />
+</template>
+
+<script setup lang="ts">
+import BaseButton from "../components/BaseButton.vue";
+
+const darkMode = () => {
+  if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem("theme", "light");
+  } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem("theme", "dark");
+  }
+}
+
+</script>
