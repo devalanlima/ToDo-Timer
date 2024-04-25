37af57737acbffe663aa65781802fd74cc5828fc
Alan Lima <dev.alanlima@gmail.com>
2024-04-25 12:32:51 -0300

    fix: Now the relative property does not directly influence the button

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,28 +1,30 @@
 <template>
   <button 
-  class="relative select-none m-1 disabled:cursor-not-allowed bg-white disabled:bg-gray-300 dark:bg-black text-black dark:text-white disabled:text-gray-500 disabled:opacity-75"
+  class="select-none m-1 disabled:cursor-not-allowed bg-white disabled:bg-gray-300 dark:bg-black text-black dark:text-white disabled:text-gray-500 disabled:opacity-75"
   @mousedown="handleMouseDown"
   @mouseup="handleMouseUp"
   @mouseleave="handleMouseLeave"
   >
-    <span 
-    :class="[
-      'border-black dark:border-white border-2 bg-inherit w-full h-full relative z-10 block py-2 px-4 transition-transform duration-75',
-      buttonIsPressed && 'translate-x-1 translate-y-1'
-    ] "
-    >
-      <span v-if="props.icon === null">
-        <slot v-if="$slots.default" />
-        <p v-else>Button</p>
+    <div class="relative bg-inherit text-inherit">
+      <span 
+      :class="[
+        'border-black dark:border-white border-2 bg-inherit w-full h-full relative z-10 block py-2 px-4 transition-transform duration-75',
+        buttonIsPressed && 'translate-x-1 translate-y-1'
+      ] "
+      >
+        <span v-if="props.icon === null">
+          <slot v-if="$slots.default" />
+          <p v-else>Button</p>
+        </span>
+        <span v-else>
+          <component 
+          is="i"
+          :class="[props.icon, 'min-w-[14px]']" 
+          />
+        </span>
       </span>
-      <span v-else>
-        <component 
-        is="i"
-        :class="props.icon" 
-        />
-      </span>
-    </span>
-    <span class="bg-black dark:bg-white block w-full h-full absolute -bottom-1 -right-1"/>
+      <span class="bg-black dark:bg-white block w-full h-full absolute -bottom-1 -right-1"/>
+    </div>
   </button>
 </template>
 
