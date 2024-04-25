5acb257ed8a1164e0bf605a2e9b56aab2cdd67a4
Alan Lima <dev.alanlima@gmail.com>
2024-04-25 12:36:50 -0300

    feat: Create borderless input

---
+++ src/components/BorderlessInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input class="cursor-text outline-none focus:border-b-2 bg-white dark:bg-black text-black dark:text-white border-black grow text-center text-xl px-2" v-model="inputValue" />
+</template>
+
+<script setup lang="ts">
+
+import { computed } from "vue";
+
+interface Props {
+  modelValue: any;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits<{
+  'update:modelValue': [value: typeof props.modelValue]
+}>()
+
+const inputValue = computed({
+  get() {
+    return props.modelValue
+  },
+  set(inputValue) {
+    emit('update:modelValue', inputValue)
+  }
+})
+
+</script>
