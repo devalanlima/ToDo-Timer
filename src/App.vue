76da7688ef7ee591f81b5dfb49e7018cb7dfb2e1
Alan Lima <dev.alanlima@gmail.com>
2024-04-25 12:44:53 -0300

    feat: Create main template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <HelloWorld/>
+  <div class="pt-10 relative">
+    <DarkmodeButton class="absolute top-1 left-4" />
+    <ul class="gap-5 grid grid-cols-[repeat(auto-fill,_minmax(500px,_1fr))] justify-items-center m-5">
+      <template v-for="item in allLists" :key="item.id">
+        <li :id="item.id" class="grow w-full">
+          <ListContainer @@delItem="delItem(item.id)" />
+        </li>
+      </template>
+    </ul>
+    <BaseButton class="fixed my-auto bottom-5 w-14 left-1/2 -translate-x-1/2" icon="fa-solid fa-plus" @@click="allLists.push({
+        name: 'Edit Title...',
+        id: v4(),
+    })"/>
+  </div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
 import { ref } from "vue";
+import ListContainer from "./components/ListContainer.vue";
+import { v4 } from "uuid";
+import BaseButton from "./components/BaseButton.vue";
+import DarkmodeButton from "./components/DarkmodeButton.vue";
+
+const allLists = ref([{
+  name: 'Edit Title...',
+  id: v4(),
+}]);
+
+const delItem = (id: string) => {
+  allLists.value = allLists.value.filter(item => item.id !== id);
+}
 
 const colorTheme = ref<string | null>(localStorage.getItem('theme'));
 
